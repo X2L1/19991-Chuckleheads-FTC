e90abc1856effa3d5b437c1edd81fca4f409e0a1
Xavier <schwartzx@go.lisd.net>
2025-12-03 19:37:57 -0600

    Removal of outdated teleop classes and an outdated auton class.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/generated/ChamberTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/generated/ChamberTester.java
@@ -1,10 +1,8 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.generated;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
-import org.firstinspires.ftc.teamcode.generated.HydraHardware;
 
 
 @TeleOp(name = "Chamber Tester", group = "TeleOp")
